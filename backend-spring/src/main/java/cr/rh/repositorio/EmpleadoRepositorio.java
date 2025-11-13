04a2463a0c219f9639e881ad109aa38b8cd82e3e
carlosninacio <cigramirez@poligran.edu.co>
2025-11-13 18:07:03 -0500

    se crea la clase entidad con jpa

---
+++ backend-spring/src/main/java/cr/rh/repositorio/EmpleadoRepositorio.java
@@ -0,0 +1,8 @@
+package cr.rh.repositorio;
+
+
+import cr.rh.modelo.Empleado;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmpleadoRepositorio extends JpaRepository<Empleado, Integer> {
+}

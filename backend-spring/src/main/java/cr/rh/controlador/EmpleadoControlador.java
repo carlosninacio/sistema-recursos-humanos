7baf56d98b64178c0134a01e8c421fb82b4e869b
carlosninacio <cigramirez@poligran.edu.co>
2025-11-13 18:31:28 -0500

    fix empleado controlador

--- backend-spring/src/main/java/cr/rh/controlador/EmpleadoControlador.java
+++ backend-spring/src/main/java/cr/rh/controlador/EmpleadoControlador.java
@@ -1,20 +1,31 @@
 package cr.rh.controlador;
 
+import cr.rh.modelo.Empleado;
 import cr.rh.servicio.EmpleadoServicio;
 import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.logging.Logger;
+import java.util.List;
 
 @RestController
 @RequestMapping("rh-app") //http://localhost:8080/rh-app/... Contexto aplicación
 @CrossOrigin
 public class EmpleadoControlador {
-    private static final Logger logger = (Logger) LoggerFactory.getLogger(EmpleadoControlador.class);
+    private static final Logger logger = LoggerFactory.getLogger(EmpleadoControlador.class);
 
     @Autowired
     private EmpleadoServicio empleadoServicio;
+
+    // http://localhost:8080/rh-app/empleados
+    @GetMapping("/empleados")
+    public List<Empleado> obtenerEmpleados() {
+        var empleados = empleadoServicio.listarEmpleados();
+        empleados.forEach(empleado -> logger.info(empleado.toString()));
+        return empleados;
+    }
 }

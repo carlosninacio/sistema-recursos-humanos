15cc10e2a6b3afd0262a80afd8b55c78ec8a4999
carlosninacio <cigramirez@poligran.edu.co>
2025-11-13 18:17:39 -0500

    implementación paquete servicio junto con su interfaz con metodos y clase con metodos desarrollados

---
+++ backend-spring/src/main/java/cr/rh/servicio/EmpleadoServicio.java
@@ -0,0 +1,35 @@
+package cr.rh.servicio;
+
+import cr.rh.modelo.Empleado;
+import cr.rh.repositorio.EmpleadoRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EmpleadoServicio implements IEmpleadoServicio {
+
+    @Autowired
+    EmpleadoRepositorio empleadoRepositorio;
+
+    @Override
+    public List<Empleado> listarEmpleados() {
+        return empleadoRepositorio.findAll();
+    }
+
+    @Override
+    public Empleado buscarEmpleadoPorId(Integer idEmpleado) {
+        return empleadoRepositorio.findById(idEmpleado).orElse(null);
+    }
+
+    @Override
+    public Empleado guardarEmpleado(Empleado empleado) {
+        return empleadoRepositorio.save(empleado);
+    }
+
+    @Override
+    public void eliminarEmpleado(Empleado empleado) {
+        empleadoRepositorio.delete(empleado);
+    }
+}

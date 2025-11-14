13c5f70e568950ee6fba500f1109625f7fd6cb55
carlosninacio <cigramirez@poligran.edu.co>
2025-11-14 09:35:59 -0500

    formulario editar empleado y recuperar y rellenar parametros del empleado a editar usando su id

--- frontend-react/src/empleados/ListadoEmpleados.js
+++ frontend-react/src/empleados/ListadoEmpleados.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import { NumericFormat } from 'react-number-format';
+import { Link } from 'react-router-dom';
 
 export default function ListadoEmpleados() {
 
@@ -30,6 +31,7 @@ export default function ListadoEmpleados() {
                         <th scope="col">Empleado</th>
                         <th scope="col">Departamento</th>
                         <th scope="col">Sueldo</th>
+                        <th></th>
                     </tr>
                 </thead>
                 <tbody>
@@ -44,7 +46,12 @@ export default function ListadoEmpleados() {
                                     displayType={"text"}
                                     thousandSeparator=',' prefix={'$'}
                                     decimalScale={2}
-                                    fixedDecimalScale /></td>
+                                    fixedDecimalScale />
+                                </td>
+                                <td className="text-center">
+                                    <Link to={`/editar/${empleado.idEmpleado}`}
+                                    className="btn btn-warning btn-sm me-3">Editar</Link>
+                                </td>
                             </tr>
                         ))
                     }

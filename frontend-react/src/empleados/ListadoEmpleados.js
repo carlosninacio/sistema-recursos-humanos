6686e22b095b5bd1147f7749eb6366534a1200af
carlosninacio <cigramirez@poligran.edu.co>
2025-11-15 12:03:02 -0500

    botón eliminar agregado

--- frontend-react/src/empleados/ListadoEmpleados.js
+++ frontend-react/src/empleados/ListadoEmpleados.js
@@ -6,7 +6,9 @@ import { Link } from 'react-router-dom';
 export default function ListadoEmpleados() {
 
     const urlBase = "http://localhost:8080/rh-app/empleados";
-    const [empleados, setEmpleados] = useState([]);
+
+    const[empleados, setEmpleados] = useState([]);
+
     useEffect(() => {
         cargarEmpleados();
     }, []);
@@ -14,8 +16,8 @@ export default function ListadoEmpleados() {
     const cargarEmpleados = async () => {
         const resultado = await axios.get(urlBase);
         console.log("Resultado cargar empleados");
-        console.log(resultado.data)
-        setEmpleados(resultado.data)
+        console.log(resultado.data);
+        setEmpleados(resultado.data);
     }
 
     const eliminarEmpleado = async (id) => {
@@ -23,47 +25,51 @@ export default function ListadoEmpleados() {
         cargarEmpleados();
     }
 
-    return (
-        <div className="container">
-            <div className="container text-center" style={{ margin: "20px" }}>
-                <h3>Sistema de Recursos Humanos</h3>
-            </div>
-
-            <table className="table table-striped table-hover align-middle">
-                <thead className="table-dark">
-                    <tr>
-                        <th scope="col">Id</th>
-                        <th scope="col">Empleado</th>
-                        <th scope="col">Departamento</th>
-                        <th scope="col">Sueldo</th>
-                        <th></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        // Iteramos el arreglo de empleados
-                        empleados.map((empleado, indice) => (
-                            <tr key={indice}>
-                                <th scope="row">{empleado.idEmpleado}</th>
-                                <td>{empleado.nombre}</td>
-                                <td>{empleado.departamento}</td>
-                                <td><NumericFormat value={empleado.sueldo}
-                                    displayType={"text"}
-                                    thousandSeparator=',' prefix={'$'}
-                                    decimalScale={2}
-                                    fixedDecimalScale />
-                                </td>
-                                <td className="text-center">
-                                    <Link to={`/editar/${empleado.idEmpleado}`}
-                                    className="btn btn-warning btn-sm me-3">Editar</Link>
-                                    <button onClick={() => eliminarEmpleado(empleado.idEmpleado)}
-                                        className='btn btn-danger btn-sm'>Eliminar</button>
-                                </td>
-                            </tr>
-                        ))
-                    }
-                </tbody>
-            </table>
+  return (
+    <div className="container">
+        <div className="container text-center" style={{margin: "30px"}}>
+            <h3>Sistema de Recursos Humanos</h3>
         </div>
-    )
+
+        <table className="table table-striped table-hover align-middle">
+        <thead className='table-dark'>
+            <tr>
+            <th scope="col">Id</th>
+            <th scope="col">Empleado</th>
+            <th scope="col">Departamento</th>
+            <th scope="col">Sueldo</th>
+            <th></th>
+            </tr>
+        </thead>
+        <tbody>
+            {
+            //Iteramos el arreglo de empleados
+            empleados.map((empleado, indice) => (
+                <tr key={indice}>
+                <th scope="row">{empleado.idEmpleado}</th>
+                <td>{empleado.nombre}</td>
+                <td>{empleado.departamento}</td>
+                <td><NumericFormat value={empleado.sueldo}
+                    displayType={'text'}
+                    thousandSeparator=',' prefix={'$'}
+                    decimalScale={2} fixedDecimalScale/>
+                </td>
+                <td className='text-center'>
+                    <div>
+                        <Link to={`/editar/${empleado.idEmpleado}`}
+                        className='btn btn-warning btn-sm me-3'>Editar</Link>
+                        <button 
+                        onClick={()=> eliminarEmpleado(empleado.idEmpleado)}
+                        className='btn btn-danger btn-sm'
+                        >Eliminar</button>
+                    </div>
+                </td>
+            </tr>
+            ))
+            }
+        </tbody>
+        </table>
+
+    </div>
+  )
 }
